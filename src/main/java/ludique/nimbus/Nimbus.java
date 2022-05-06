5892c9e6e9f451e85c6056c4b5ae391449912292
Raphaël Pion <raph.pion@gmail.com>
2022-05-06 07:59:43 -0400

    Add basefiles

---
+++ src/main/java/ludique/nimbus/Nimbus.java
@@ -0,0 +1,17 @@
+package ludique.nimbus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nimbus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
